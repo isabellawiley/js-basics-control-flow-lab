258bbd11daa49fc184494037ae30f49388b00260
Isabella <wileyisabella@gmail.com>
2021-03-15 13:10:32 -0400

    commit tree 3c162992e5fa87986d800b67dc4d81fb0cd27e29

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(num){
   // Write your code here!
+  const ride = num;
+  if (ride <= 400){
+    return "This one is on me!";
+  }
+  else if(ride > 2000 && ride <= 2500){
+    return "I will gladly take your thirty bucks.";
+  }
+  else if(ride > 2500){
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(string){
   // Write your code here!
+  const city = string;
+  if (city == "NYC"){
+    return "Ok, sounds good.";
+  }
+  else {
+    return "No go.";
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(num){
   // Write your code here!
+  const tip = num;
+  if (tip == "generous"){
+    return "Thank you so much."
+  }
+  else if(tip == "not as generous"){
+    return "Thank you.";
+  }
+  else{
+    return "Bye.";
+  }
 }
